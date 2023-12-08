38efbc5d2ca468cd2a7152eb8ad9c71ad1e0e219
Shubhadeep Mahato <shubhadeepmahato123@gmail.com>
2023-12-08 20:19:50 +0530

    Founder image updated

--- src/components/Founder.jsx
+++ src/components/Founder.jsx
@@ -5,7 +5,7 @@ import { FaInstagram, FaLinkedinIn,FaTwitter } from "react-icons/fa";
 // Define the founder data
 const founder = {
   name: "Shubhadeep Mahato",
-  image: "https://scontent.fccu9-3.fna.fbcdn.net/v/t39.30808-6/326110770_694534842140821_5042788323370012458_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=vkJdsv9tB9AAX_T_ULi&_nc_ht=scontent.fccu9-3.fna&oh=00_AfAGrGgZPTdG80em1yfFwWAWmv3J_LZcpWCEuzfSST30Og&oe=655D341A",
+  image: "http://bit.ly/3RgSMHs",
   bio: "Hi I am Shubhadeep Mahato The Founder And CEO Of DropNotch.",
   socials: [
     {
